53d993a1f8c5258d8dbe646c0299521b06f71551
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-04-22 23:03:43 +0200

    New books

--- bookworm/src/Components/Books.tsx
+++ bookworm/src/Components/Books.tsx
@@ -33,9 +33,9 @@ let Books = [
         "id": 4,
         "autor": "Haruki Murakami",
         "tytul": "Norwegian Wood",
-        "rating": 4,
+        "rating": 0,
         "strony": 296,
-        "strona": 100,
+        "strona": 0,
         "status": "Planowane",
         "img": "https://tajfuny.pl/wp-content/uploads/2021/05/norwegian-wood.jpeg"
     },
@@ -63,9 +63,9 @@ let Books = [
         "id": 7,
         "autor": "Jane Austen",
         "tytul": "Pride and Prejudice",
-        "rating": 4,
+        "rating": 0,
         "strony": 432,
-        "strona": 200,
+        "strona": 0,
         "status": "Planowane",
         "img": "https://austenprose.files.wordpress.com/2010/07/pride-and-prejudice-barnes-nobel-2015.jpg"
       },
@@ -83,9 +83,9 @@ let Books = [
         "id": 9,
         "autor": "Ernest Hemingway",
         "tytul": "The Old Man and the Sea",
-        "rating": 4,
+        "rating": 0,
         "strony": 127,
-        "strona": 70,
+        "strona": 0,
         "status": "Planowane",
         "img": "https://m.media-amazon.com/images/I/71Au8SjrwkL._AC_UF1000,1000_QL80_.jpg"
       },
@@ -93,9 +93,9 @@ let Books = [
         "id": 10,
         "autor": "J.K. Rowling",
         "tytul": "Harry Potter and the Philosopher's Stone",
-        "rating": 5,
+        "rating": 0,
         "strony": 223,
-        "strona": 223,
+        "strona": 0,
         "status": "Planowane",
         "img": "https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_568,c_scale/jackets/9781408855652.jpg"
       },
@@ -107,7 +107,88 @@ let Books = [
         "strony": 1440,
         "strona": 700,
         "status": "Porzucone",
-        "img": "https://m.media-amazon.com/images/I/A1aDb5U5myL.jpg"
-      },      
+        "img": "https://m.media-amazon.com/images/I/A1aDb5U5myL.jpg",
+        
+      },    
+      {
+        "id": 12,
+        "autor": "Neil Gaiman",
+        "tytul": "American Gods",
+        "rating": 4,
+        "strony": 635,
+        "strona": 150,
+        "status": "Czytane",
+        "img": "https://m.media-amazon.com/images/I/51Pb-OAREFL._AC_UF1000,1000_QL80_.jpg"
+        },
+        {
+            "id": 13,
+            "autor": "Gillian Flynn",
+            "tytul": "Gone Girl",
+            "rating": 5,
+            "strony": 419,
+            "strona": 419,
+            "status": "Przeczytane",
+            "img": "https://upload.wikimedia.org/wikipedia/en/thumb/7/7e/Gone_Girl_%28Flynn_novel%29.jpg/220px-Gone_Girl_%28Flynn_novel%29.jpg"
+            },
+            {
+            "id": 14,
+            "autor": "Kazuo Ishiguro",
+            "tytul": "Never Let Me Go",
+            "rating": 5,
+            "strony": 288,
+            "strona": 100,
+            "status": "Czytane",
+            "img": "https://assets.fontsinuse.com/static/use-media-items/59/58176/full-938x1423/59d69821/81cLIU6t99L.jpeg"
+            },
+            {
+            "id": 15,
+            "autor": "Chimamanda Ngozi Adichie",
+            "tytul": "Half of a Yellow Sun",
+            "rating": 0,
+            "strony": 543,
+            "strona": 0,
+            "status": "Planowane",
+            "img": "https://images.penguinrandomhouse.com/cover/9780307272300"
+            },
+            {
+                "id": 16,
+                "autor": "Octavia Butler",
+                "tytul": "Kindred",
+                "rating": 5,
+                "strony": 264,
+                "strona": 150,
+                "status": "Czytane",
+                "img": "https://images.penguinrandomhouse.com/cover/9780807083697"
+                },
+                {
+                "id": 17,
+                "autor": "Fyodor Dostoevsky",
+                "tytul": "Crime and Punishment",
+                "rating": 0,
+                "strony": 671,
+                "strona": 0,
+                "status": "Planowane",
+                "img": "https://upload.wikimedia.org/wikipedia/en/4/4b/Crimeandpunishmentcover.png"
+                },
+                {
+                "id": 18,
+                "autor": "Toni Morrison",
+                "tytul": "Beloved",
+                "rating": 5,
+                "strony": 321,
+                "strona": 100,
+                "status": "Czytane",
+                "img": "https://images.penguinrandomhouse.com/cover/9781400033416"
+                },
+                {
+                    "id": 19,
+                    "autor": "J.D. Salinger",
+                    "tytul": "The Catcher in the Rye",
+                    "rating": 0,
+                    "strony": 224,
+                    "strona": 0,
+                    "status": "Planowane",
+                    "img": "https://m.media-amazon.com/images/I/91HPG31dTwL.jpg"
+                    },  
 ]
 export default Books
