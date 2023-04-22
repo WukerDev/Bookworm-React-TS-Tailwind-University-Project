3c39a01711f922ba1ff11e31b6ecb8c7f737b2ac
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-04-22 13:22:47 +0200

    Zmiany

--- bookworm/src/routes/home.tsx
+++ bookworm/src/routes/home.tsx
@@ -1,12 +1,11 @@
 import Imiona from "../Components/BazaImion"
 
-
-
+import { Tabs, Card, Button } from "flowbite-react";
 
 export default function Home() {
   return (
     <>
-    <div className="justify-start h-screen px-2 py-2 m-2 flex-col flex bg-slate-200">
+    <div className="justify-start h-screen px-2 py-2 m-2 flex-col flex bg-slate-200 gap-1">
     <form className="flex items-center">   
     <label htmlFor="simple-search" className="sr-only">Wyszukaj</label>
     <div className="relative w-full">
@@ -20,21 +19,45 @@ export default function Home() {
         <span className="sr-only">Wyszukaj</span>
     </button>
 </form>
-<p className="h-2"></p>
-   <ul className="flex flex-wrap text-sm font-medium text-center justify-center text-gray-500 dark:text-gray-400">
-    <li className="mr-2"> 
-        <a href="#" className="inline-block px-2 py-2 text-white bg-blue-600 rounded-lg active" aria-current="page">Czytam</a>
+<div className="sm:hidden">
+    <label htmlFor="tabs" className="sr-only">Select your country</label>
+    <select id="tabs" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option>Czytane</option>
+        <option>Planowane</option>
+        <option>Ukończone</option>
+        <option>Przerwane</option>
+        <option>Porzucone</option>
+    </select>
+</div>
+<ul className="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
+    <li className="w-full">
+        <a href="#" className="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">Profile</a>
     </li>
-    <li className="mr-2">
-        <a href="#"  className="inline-block px-2 py-2 rounded-lg hover:text-gray-900  hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Planuje</a>
+    <li className="w-full">
+        <a href="#" className="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Dashboard</a>
     </li>
-    <li className="mr-2">
-        <a href="#" className="inline-block px-2 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Skończone</a>
+    <li className="w-full">
+        <a href="#" className="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Settings</a>
     </li>
-    <li className="mr-2">
-        <a href="#" className="inline-block px-2 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Porzucone</a>
+    <li className="w-full">
+        <a href="#" className="inline-block w-full p-4 bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Invoice</a>
     </li>
 </ul>
+
+<div className="max-w-sm">
+  <Card
+    horizontal={true}
+    imgSrc="https://flowbite.com/docs/images/blog/image-4.jpg"
+  >
+    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      Noteworthy technology acquisitions 2021
+    </h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+  </Card>
+</div>
+
 </div>
     </>
   )
