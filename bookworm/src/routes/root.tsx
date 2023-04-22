a2885feef4af81b0efc645a77db74c97962f8353
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-04-22 11:38:19 +0200

    Zmiana

--- bookworm/src/routes/root.tsx
+++ bookworm/src/routes/root.tsx
@@ -8,7 +8,7 @@ export default function Root() {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count + 1);
   useEffect(() => {
-    document.title = `Astral | ${Imiona[0].imie}, ${Imiona[1].imie}, ${Imiona[2].imie}`;
+    document.title = `Bookworm By Wiktor & Sebastian`;
   });
     return (
         <>
