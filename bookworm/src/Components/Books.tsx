1b289b4af86c0abda1d1454a6edc3f0ddcfbc697
Don <sebek4949@gmail.com>
2023-04-22 20:28:29 +0200

    Books new records

--- bookworm/src/Components/Books.tsx
+++ bookworm/src/Components/Books.tsx
@@ -1,13 +1,63 @@
 let Books = [
     {
         "id": 1,
-        "autor": "Wiktor Kozakowski",
-        "tytul": "Wiedźmin",
-        "rating": 5,
-        "strony": 100,
-        "strona": 1,
+        "autor": "J.R.R. Tolkien",
+        "tytul": "The Lord of the Rings",
+        "rating": 4,
+        "strony": 1178,
+        "strona": 347,
         "status": "Czytane",
         "img": "https://edit.org/images/cat/book-covers-big-2019101610.jpg"
-    }
+    },
+    {
+        "id": 2,
+        "autor": "George R.R. Martin",
+        "tytul": "A Game of Thrones",
+        "rating": 4,
+        "strony": 694,
+        "strona": 694,
+        "status": "Przeczytane",
+        "img": "https://edit.org/images/cat/book-covers-big-2019101610.jpg"
+    },
+    {
+        "id": 3,
+        "autor": "Margaret Atwood",
+        "tytul": "The Handmaid's Tale",
+        "rating": 5,
+        "strony": 311,
+        "strona": 145,
+        "status": "Czytane",
+        "img": "https://edit.org/images/cat/book-covers-big-2019101610.jpg"
+    },
+    {
+        "id": 4,
+        "autor": "Haruki Murakami",
+        "tytul": "Norwegian Wood",
+        "rating": 4,
+        "strony": 296,
+        "strona": 100,
+        "status": "Czytane",
+        "img": "https://edit.org/images/cat/book-covers-big-2019101610.jpg"
+    },
+    {
+        "id": 5,
+        "autor": "Stephen King",
+        "tytul": "The Shining",
+        "rating": 5,
+        "strony": 447,
+        "strona": 210,
+        "status": "Czytane",
+        "img": "https://edit.org/images/cat/book-covers-big-2019101610.jpg"
+    },
+    {
+        "id": 6,
+        "autor": "Hermann Hesse",
+        "tytul": "Siddhartha",
+        "rating": 5,
+        "strony": 152,
+        "strona": 75,
+        "status": "Czytane",
+        "img": "https://edit.org/images/cat/book-covers-big-2019101610.jpg"
+    },
 ]
 export default Books
