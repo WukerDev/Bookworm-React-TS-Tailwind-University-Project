8280ea41a0bd3f3d76113fe8c4732214cdb07ec5
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-04-22 11:31:46 +0200

    tabs renamed

--- bookworm/src/routes/home.tsx
+++ bookworm/src/routes/home.tsx
@@ -6,21 +6,18 @@ import Imiona from "../Components/BazaImion"
 export default function Home() {
   return (
     <>
-   <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-    <li className="mr-2">
-        <a href="#" className="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active" aria-current="page">Tab 1</a>
+   <ul className="flex flex-wrap text-sm font-medium text-center justify-center text-gray-500 dark:text-gray-400">
+    <li className="mr-2"> 
+        <a href="#" className="inline-block px-2 py-2 text-white bg-blue-600 rounded-lg active" aria-current="page">Czytam</a>
     </li>
     <li className="mr-2">
-        <a href="#"  className="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab 2</a>
+        <a href="#"  className="inline-block px-2 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Planuje</a>
     </li>
     <li className="mr-2">
-        <a href="#" className="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab 3</a>
+        <a href="#" className="inline-block px-2 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Skończone</a>
     </li>
     <li className="mr-2">
-        <a href="#" className="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab 4</a>
-    </li>
-    <li>
-        <a className="inline-block px-4 py-3 text-gray-400 cursor-not-allowed dark:text-gray-500">Tab 5</a>
+        <a href="#" className="inline-block px-2 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Porzucone</a>
     </li>
 </ul>
     </>
