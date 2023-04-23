0d57d27750ff7148dd5a506620fec538fe0e0d87
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-04-23 17:35:10 +0200

    Drop shadow

--- bookworm/src/Components/Menu.tsx
+++ bookworm/src/Components/Menu.tsx
@@ -65,17 +65,17 @@ const BookList = () => {
   return (
     <>
     <form onSubmit={handleButtonClick}> {/* add onSubmit handler */}
-        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only  dark:text-white">Search</label>
         <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
           </div>
-          <input type="text" value={searchTerm} onChange={handleInputChange} className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Wyszukaj książkę..." required/>
+          <input type="text" value={searchTerm} onChange={handleInputChange} className="block w-full p-4 pl-10 text-sm text-gray-900 border shadow-lg border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Wyszukaj książkę..." required/>
           <button type="submit"  className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Wyszukaj</button>
         </div>
       </form>
     <div>
-    <ul className="grid grid-flow-col text-center text-gray-500 bg-white rounded-lg  border border-gray-300 ">
+    <ul className="grid grid-flow-col text-center text-gray-500 bg-white rounded-lg  border border-gray-300 shadow-lg ">
       <li>
         <a
          
@@ -124,7 +124,7 @@ const BookList = () => {
       {buttonClicked && searchResults.length > 0 ? (
           <div id="content" className="flex flex-wrap justify-center pt-1 ">
           {searchResults.map(book => (
-            <div id="debug" className="bg-white rounded-xl  w-80 flex flex-start my-1 mx-1 border border-gray-300" key={book.id}>
+            <div id="debug" className="bg-white rounded-xl  w-80 flex flex-start my-1 mx-1 border border-gray-300 shadow-lg" key={book.id}>
               <img src={book.img} className="w-28 rounded-xl"/>
               <div className="w-4/5 justify-center flex flex-col py-5 ">
                 <p className="font-light text-slate-500 text-s">{book.autor}</p>
@@ -148,7 +148,7 @@ const BookList = () => {
       ) : (
         <div id="content" className="flex flex-wrap justify-center pt-1">
         {books.map(book =>  (
-          <div className="bg-white rounded-xl w-80 flex flex-start my-1 mx-1 border border-gray-300" key={book.id}>
+          <div className="bg-white rounded-xl w-80 flex flex-start my-1 mx-1 border border-gray-300 shadow-lg" key={book.id}>
             <img src={book.img} className="w-28 rounded-xl"/>
             <div className="w-4/5 justify-center flex flex-col py-5">
               <p className="font-light text-slate-500 text-s">{book.autor}</p>
