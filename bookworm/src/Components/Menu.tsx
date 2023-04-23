10e610a07bf747653ec0e5c0e54b53bfc1a4468d
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-04-23 21:35:59 +0200

    Plus button

--- bookworm/src/Components/Menu.tsx
+++ bookworm/src/Components/Menu.tsx
@@ -191,7 +191,12 @@ const BookList = () => {
       </div>
       )}
 </div>
-</>
+<button type="submit"  className="text-white fixed bottom-3 right-5  p-0 w-16 h-16 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg viewBox="0 0 20 20" enable-background="new 0 0 20 20" className="w-10 h-10 inline-block">
+            <path fill="#FFFFFF" d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
+                                    C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
+                                    C15.952,9,16,9.447,16,10z" />
+          </svg></button>
+         </>
   );
 };
 
